7e7d36dacb776156d5a29461bd8285b4b26c8468
NikitaNikitchenko <62266898+NikitaNikitchenko@users.noreply.github.com>
2023-04-13 21:55:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,12 @@
+
 import pandas as pd
 import numpy as np
+from scipy.stats import  mannwhitneyu
 
+chat_id = 7846643586 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(...) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
+def solution(x, y) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    return mannwhitneyu(x, y, alternative='greater').pvalue < 0.09 # Ваш ответ, True или False
